c7ff50541fcae714d5e9e8ad639be04719a19965
Vavarine <Vavarine@users.noreply.github.com>
2021-02-27 13:40:17 -0300

    Debug

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,7 @@
 import Head from 'next/head';
 
 import CompletedChallenges from '../components/CompletedChallenges';
-import Countdown from '../components/CountDown';
+import Countdown from '../components/Countdown';
 import ExperienceBar from '../components/ExperienceBar';
 import Profile from '../components/Profile';
 
